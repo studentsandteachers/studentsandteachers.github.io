d229c2c6a758ccc73e99e32748d522c353589de4
Rob <rjamie2070@gmail.com>
2015-05-18 08:59:41 -0700

    fixing more stuff that people messed up....

--- js/game.js
+++ js/game.js
@@ -4,42 +4,42 @@ var computerChoice;
 var score = 0;
 
 function playerRock(){
-<<<<<<< HEAD:js/game.js
+		 HEAD:js/game.js
 		//Set The Image & Text
 		document.getElementById("player").innerHTML = "<h3>Rock</h3><img src='img/rock.png'>"
 		//Set playerChoice variable to Rock
 		playerChoice = 1;
 	// Run Computer's Turn, Randomize
-=======
+
 	document.getElementById("player").innerHTML = "<h3>Rock</h3><img src='pictures/rock.png'>"
 	playerChoice = 1;
->>>>>>> origin/master:game.js
+	 origin/master:game.js
 	randomize();
 }
 function playerPaper(){
-<<<<<<< HEAD:js/game.js
+	 HEAD:js/game.js
 		//Set The Image & Text
 		document.getElementById("player").innerHTML = "<h3>Paper</h3><img src='img/paper.png'>"
 		//Set playerChoice variable to Paper
 		playerChoice = 2;
 	// Run Computer's Turn, Randomize
-=======
+
 	document.getElementById("player").innerHTML = "<h3>Paper</h3><img src='pictures/paper.png'>"
 	playerChoice = 2;
->>>>>>> origin/master:game.js
+	origin/master:game.js
 	randomize();
 }
 function playerScissors(){
-<<<<<<< HEAD:js/game.js
+	 HEAD:js/game.js
 		//Set The Image & Text
 		document.getElementById("player").innerHTML = "<h3>Scissors</h3><img src='img/scissors.png'>"
 		//Set playerChoice variable to Scissors
 		playerChoice = 3;
 	// Run Computer's Turn, Randomize
-=======
+
 	document.getElementById("player").innerHTML = "<h3>Scissors</h3><img src='pictures/scissors.png'>"
 	playerChoice = 3;
->>>>>>> origin/master:game.js
+	 origin/master:game.js
 	randomize();
 }
 function randomize(){
@@ -54,16 +54,16 @@ function computer(){
 		computerChoice = 1;
 	} 
 	else if (randomNumber <= 66){
-<<<<<<< HEAD:js/game.js
+ HEAD:js/game.js
 		//Paper
 			console.log('Paper.');
 			document.getElementById("computer").innerHTML = "<h3>Paper</h3><img src='img/paper.png'>"
 			computerChoice = 2;
-=======
+
 		console.log('Paper.');
 		document.getElementById("computer").innerHTML = "<h3>Paper</h3><img src='pictures/paper.png'>"
 		computerChoice = 2;
->>>>>>> origin/master:game.js
+	 origin/master:game.js
 	} 
 	else if (randomNumber <= 100){
 		console.log('Scissors.');
